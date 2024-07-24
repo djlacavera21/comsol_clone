ca83c5e40f7fec07c7fd5d1ab620b055d058b2f9
djlacavera21 <djlacavera21@gmail.com>
2024-07-24 13:59:31 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# COMSOL Clone
+
+A free software version of COMSOL MultiPhysics Simulator.
+
+## Overview
+
+COMSOL Clone is a multiphysics simulation tool that allows users to perform various types of physics simulations, including Poisson's equation, elasticity, and heat transfer problems. This project aims to provide an open-source alternative to COMSOL MultiPhysics.
+
+## Features
+
+- **Poisson's Equation**: Solve Poisson's equation in 2D.
+- **Structural Mechanics**: Perform elasticity analysis for 2D structures.
+- **Heat Transfer**: Simulate heat conduction in 2D.
+
+## Installation
+
+To install the COMSOL Clone, you need Python 3.6 or higher. You can install the required dependencies using pip:
+
+```sh
+pip install numpy scipy matplotlib PyQt5
+
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='comsol_clone',
+    version='0.1',
+    author='Dominic James LaCavera Jr./PythonGPT',
+    author_email='Djlacavera21@gmail.com',
+    description='A free software version of COMSOL MultiPhysics Simulator',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/comsol_clone',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'PyQt5',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'comsol_clone = gui.main_window:main',
+        ],
+    },
+)
+
